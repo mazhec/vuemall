f9fc61297068a217437f8ef2afd3f6610b3e628b
马喆昌 <7493508+mazhechang@user.noreply.gitee.com>
2020-11-28 15:43:41 +0800

    第二次提交

---
+++ src/views/home/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物车</div>
+    </nav-bar>
+  </div>
+</template>
+
+<script>
+  import NavBar from "components/common/navbar/NavBar.vue";
+
+  import { getHomeMultidata } from "network/home";
+  export default {
+    components: { NavBar },
+    name: "Home",
+    data() {
+      return {
+        banners: [],
+        recommends: []
+      }
+    },
+    created() {
+      getHomeMultidata().then(res => {
+        this.banners = res.data.banner.list
+        this.recommends = res.data.recommend.list
+      })
+    }
+  };
+</script>
+
+<style scoped>
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
+</style>
